499a72e0e9e2d5992a044da1ec8e4947bce6f4b0
Shazia252 <161409243+Shazia252@users.noreply.github.com>
2024-04-19 19:54:35 -0700

    Adding a Word Counter Project

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+// Import the 'inquirer' and 'chalk' module, which is a command line interface for Node.js
+import inquirer from "inquirer";
+import chalk from "chalk";
+// Print the colorful welcome message
+console.log(chalk.bold.cyanBright("\n \t\t Code with Shazia - Word Counter"));
+console.log("=".repeat(60));
+// Decline a constant  'answer' and assigin it to the ressult of inquirer.prompt function
+
+const answers: {
+    Sentence: string 
+} = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        massage: "Enter your sentence to count the word:"
+    }  
+])
+
+const words = answers.Sentence.trim().split(" ")
+
+// Print the array of words to the console
+console.log("=".repeat(60));
+console.log(chalk.bold("- Sentence Words:"));
+console.log(words);
+console.log(chalk.bold(`\n - Your sentance word count is: ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(60));
